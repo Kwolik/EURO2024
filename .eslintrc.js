18f01b9317e14fb5d8bba918f157cbd37345d7e1
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-04 17:08:53 +0200

    Change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    root: true,
-    extends: [
-      'eslint:recommended',
-    ],
-    rules: {
-      // Ensures props and state inside functions are always up-to-date
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-  };
+  root: true,
+  extends: ["airbnb"],
+  rules: {
+    // Ensures props and state inside functions are always up-to-date
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  plugins: ["react"],
+};
+
