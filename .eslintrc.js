d7dd52a4223643b41e7bc07c7f1fa55d98480d00
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-04 16:10:17 +0200

    fixed bugs v3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     extends: [
-      'universe/native',
+      'eslint:recommended',
     ],
     rules: {
       // Ensures props and state inside functions are always up-to-date
