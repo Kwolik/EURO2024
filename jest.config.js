61fb4df4532f7b07d3209a34aca0707a6bdb0121
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-04 18:52:31 +0200

    example

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,4 @@
-const { defaults } = require("jest-config");
-
-/** @type {import('jest').Config} */
-const config = {
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-  moduleFileExtensions: [
-    "js",
-    "jsx",
-    "ts",
-    "tsx",
-    "json",
-    "node",
-    "mts",
-    "cts",
-  ],
-};
-
-module.exports = config;
+module.exports = {
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node', 'mts', 'cts'],
+  };
