6f82d4904ef0f2809cda5376435680a662c27c1a
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-04 19:16:24 +0200

    Delete 1 test

--- jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node', 'mts', 'cts'],
-  };
