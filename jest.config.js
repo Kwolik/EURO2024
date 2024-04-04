2977fc88438d9b2bb6c0a591558e9248c005ad02
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-04 18:41:51 +0200

    Fixed file

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-const {defaults} = require('jest-config');
+const { defaults } = require("jest-config");
 
 /** @type {import('jest').Config} */
 const config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts', 'cts'],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node",
+    "mts",
+    "cts",
+  ],
 };
 
 module.exports = config;
