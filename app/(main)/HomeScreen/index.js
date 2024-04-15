7c79d9a39b7e22a7252f23cf8814efb61ce73d63
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-15 14:22:08 +0200

    Create component King Footballer Row

--- app/(main)/HomeScreen/index.js
+++ app/(main)/HomeScreen/index.js
@@ -1,4 +1,4 @@
-import { View, ImageBackground, Text } from "react-native";
+import { View, ImageBackground, Text, ScrollView } from "react-native";
 import React from "react";
 import styles from "./styles.js";
 import NextMatch from "../../../components/NextMatch/index.js";
@@ -6,6 +6,7 @@ import TopRanked from "../../../components/TopRanked/index.js";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import RowMatch from "../../../components/RowMatch/index.js";
 import ChampionRow from "../../../components/ChampionRow/index.js";
+import KingFootballerRow from "../../../components/KingFootballerRow/index.js";
 
 export default function HomeScreen() {
   return (
@@ -14,47 +15,53 @@ export default function HomeScreen() {
         source={require("../../../assets/background.jpg")}
         style={styles.image}
       >
-        <View>
-          <NextMatch />
-        </View>
-        <View>
-          <TopRanked />
-        </View>
-        <View style={styles.plates}>
-          <View style={styles.plate}>
-            <View style={styles.icon}>
-              <Ionicons name="football-outline" style={styles.iconMain} />
+        <ScrollView>
+          <View>
+            <NextMatch />
+          </View>
+          <View>
+            <TopRanked />
+          </View>
+          <View style={styles.plates}>
+            <View style={styles.plate}>
+              <View style={styles.icon}>
+                <Ionicons name="football-outline" style={styles.iconMain} />
+              </View>
+              <View style={styles.bottom}>
+                <Text style={styles.name}>Mecze</Text>
+              </View>
             </View>
-            <View style={styles.bottom}>
-              <Text style={styles.name}>Mecze</Text>
+            <View style={styles.plate}>
+              <View style={styles.icon}>
+                <Ionicons name="star-outline" style={styles.iconMain} />
+              </View>
+              <View style={styles.bottom}>
+                <Text style={styles.name}>Ranking</Text>
+              </View>
+            </View>
+            <View style={styles.plate}>
+              <View style={styles.icon}>
+                <Ionicons name="settings-outline" style={styles.iconMain} />
+              </View>
+              <View style={styles.bottom}>
+                <Text style={styles.name}>Profil</Text>
+              </View>
             </View>
           </View>
-          <View style={styles.plate}>
-            <View style={styles.icon}>
-              <Ionicons name="star-outline" style={styles.iconMain} />
-            </View>
-            <View style={styles.bottom}>
-              <Text style={styles.name}>Ranking</Text>
-            </View>
+          <View>
+            <RowMatch />
+            <RowMatch />
+            <RowMatch />
           </View>
-          <View style={styles.plate}>
-            <View style={styles.icon}>
-              <Ionicons name="settings-outline" style={styles.iconMain} />
-            </View>
-            <View style={styles.bottom}>
-              <Text style={styles.name}>Profil</Text>
-            </View>
+          <View>
+            <ChampionRow />
+            <ChampionRow />
           </View>
-        </View>
-        <View>
-          <RowMatch />
-          <RowMatch />
-          <RowMatch />
-        </View>
-        <View>
-          <ChampionRow />
-          <ChampionRow />
-        </View>
+          <View>
+            <KingFootballerRow />
+            <KingFootballerRow />
+          </View>
+        </ScrollView>
       </ImageBackground>
     </View>
   );
