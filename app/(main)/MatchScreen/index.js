2538dd3e9da9ff493627d3fdfc82684eb34a9570
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-17 14:48:58 +0200

    Configuration Match screen

--- app/(main)/MatchScreen/index.js
+++ app/(main)/MatchScreen/index.js
@@ -1,20 +1,48 @@
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
 import React from "react";
 import styles from "./styles.js";
-import NextMatch from "../../../components/NextMatch/index.js";
+import CountryFlag from "react-native-country-flag";
+import Player from "../../../components/Player/index.js";
 
 export default function MatchScreen() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("../../../assets/background.jpg")}
+        source={require("../../../assets/backgroundMatch.jpg")}
         style={styles.image}
       >
         <View>
-          <NextMatch />
+          <View style={styles.top}>
+            <View style={styles.info}>
+              <Text style={styles.date}>14.06</Text>
+              <Text style={styles.date}>17:00</Text>
+            </View>
+            <View style={styles.mainTeams}>
+              <CountryFlag isoCode="fr" size={46} />
+              <Text style={styles.result}>2 : 0</Text>
+              <CountryFlag isoCode="pl" size={46} />
+            </View>
+          </View>
+          <View style={styles.bottom}>
+            <Text style={styles.teams}>Francja - Polska</Text>
+          </View>
         </View>
-        <View>
+        <View style={styles.players}>
+          <Player />
+          <Player />
+          <Player />
         </View>
+        <View style={styles.players}>
+          <Player />
+          <Player />
+          <Player />
+        </View>
+        <View style={styles.players}>
+          <Player />
+          <Player />
+          <Player />
+        </View>
+        <View></View>
       </ImageBackground>
     </View>
   );
--- app/(main)/MatchScreen/styles.js
+++ app/(main)/MatchScreen/styles.js
@@ -8,6 +8,64 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "auto", //upewnic sie czy background dobrze sie skaluje
   },
+  top: {
+    backgroundColor: "#003279",
+    height: 72,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    zIndex: 1,
+  },
+  info: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  date: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  mainTeams: {
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: -8,
+  },
+  result: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  bottom: {
+    height: 72,
+    backgroundColor: "#FFFFFF",
+    marginTop: -44,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    marginBottom: 36,
+  },
+  teams: {
+    color: "#003279",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 2,
+  },
+  players: {
+    borderColor: "green",
+    borderWidth: 2,
+
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    margin: 10,
+  },
 });
 
 export default styles;
