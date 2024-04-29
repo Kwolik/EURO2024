67ef74cc6d5841b3d6b4888e47429aefa7bbc9f8
Robert Kwoll <robert.kwoll@itsilesia.com>
2024-04-29 12:39:05 +0200

    Configuration expo image picker

--- app/(main)/SettingsScreen/index.js
+++ app/(main)/SettingsScreen/index.js
@@ -1,21 +1,48 @@
 import { View, ImageBackground, Text, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.js";
 import Foundation from "react-native-vector-icons/Foundation";
 import RowMatch from "../../../components/RowMatch/index.js";
+import * as ImagePicker from "expo-image-picker";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function MatchesScreen() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <ImageBackground
         source={require("../../../assets/background.jpg")}
         style={styles.image}
       >
         <View style={styles.profile}>
-          <Image
-            style={styles.avatar}
-            source={require("../../../assets/EURO2024logo.png")}
-          />
+          <TouchableOpacity onPress={() => pickImage()} style={styles.button}>
+            {image ? (
+              <Image style={styles.avatar} source={{ uri: image }} />
+            ) : (
+              <Image
+                style={styles.avatar}
+                source={require("../../../assets/EURO2024logo.png")}
+              />
+            )}
+          </TouchableOpacity>
           <View style={styles.top}>
             <View style={styles.space}></View>
             <Text style={styles.nick}>elPulokogftyd</Text>
@@ -52,6 +79,6 @@ export default function MatchesScreen() {
           <RowMatch />
         </View>
       </ImageBackground>
-    </View>
+    </GestureHandlerRootView>
   );
 }
